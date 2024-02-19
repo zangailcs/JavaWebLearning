ce14525f764c7a11eaa68f45aeca9e49a081557c
lics <1183706328@qq.com>
2024-02-19 22:22:30 +0800

    vue-elementui-table

--- 03_vue/vue-project/src/views/element/ElementView.vue
+++ 03_vue/vue-project/src/views/element/ElementView.vue
@@ -9,17 +9,45 @@
             <el-button type="warning">警告按钮</el-button>
             <el-button type="danger">危险按钮</el-button>
         </el-row>
+
+        <!-- 表格 -->
+        <el-table :data="tableData" border style="width: 100%">
+            <el-table-column prop="date" label="日期" width="180">
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="180">
+            </el-table-column>
+            <el-table-column prop="address" label="地址">
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            tableData: [{
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1517 弄'
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1519 弄'
+            }, {
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1516 弄'
+            }]
+        }
+    }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
 
 
