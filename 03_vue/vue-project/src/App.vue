fed1110ad6c3ba0ecf07d90742600c16c5e9ef20
lics <1183706328@qq.com>
2024-02-19 22:13:43 +0800

    vue-first

---
+++ 03_vue/vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- <h1>{{message}}</h1> -->
+    <ElementView></ElementView>
+  </div>
+</template>
+
+<script>
+import ElementView from './views/element/ElementView.vue'
+export default {
+  components: {ElementView},
+  data() {
+    return {
+      message: "Hello vue"
+    }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
