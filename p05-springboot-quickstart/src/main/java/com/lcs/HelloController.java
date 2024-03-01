5665916e0e4bf24fba387680e4c2dccd6ade7cf1
lics <1183706328@qq.com>
2024-03-01 22:24:55 +0800

    springboot-入门程序

--- p05-springboot-quickstart/src/main/java/com/lcs/HelloController.java
+++ p05-springboot-quickstart/src/main/java/com/lcs/HelloController.java
@@ -1,6 +1,8 @@
 package com.lcs;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 请求处理类
@@ -11,4 +13,27 @@ public class HelloController {
     public String hello() {
         return "Hello SpringBoot!";
     }
+
+    @RequestMapping("/simpleParam")
+    public String simpleParam(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String ageStr = request.getParameter("age");
+        int age = Integer.parseInt(ageStr);
+        System.out.println(name + ":" + age);
+        return "Hello, simpleParam: " + name + ":" + age;
+    }
+
+    /**
+     * 简单参数示例
+     *
+     * @param userName 字符串 演示RequestParam；默认required=true，不填报错400
+     * @param age 整形，封装类型好处：不传时不会报错，如果用int，不传时报错500
+     * @return str
+     */
+    @RequestMapping("/simpleParamBySpringBoot")
+    public String simpleParamBySpringBoot(@RequestParam(name = "name", required = false) String userName, Integer age) {
+        System.out.println(userName + ":" + age);
+        return "Hello, simpleParamBySpringBoot: " + userName + ":" + age;
+    }
+
 }
